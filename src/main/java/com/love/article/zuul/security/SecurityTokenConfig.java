3d6ca619711eae318435abc0e69e8cc7d49912ee
fedexu <federico.xu125@gmail.com>
2020-02-24 23:53:11 +0100

    added security layer

---
+++ src/main/java/com/love/article/zuul/security/SecurityTokenConfig.java
@@ -0,0 +1,42 @@
+package com.love.article.zuul.security;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private JwtConfig jwtConfig;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
+				// Add a filter to validate the tokens with every request
+				.addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
+				// authorization requests config
+				.authorizeRequests()
+				// allow all who are accessing "auth" service
+				.antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
+				// Any other request must be authenticated
+				.anyRequest().authenticated();
+				// At gateway level we want only to have an autorized user access the API GATEWAY. 
+				// Any other profiled authentication will be done by services iteself
+	}
+
+	@Bean
+	public JwtConfig jwtConfig() {
+		return new JwtConfig();
+	}
+}
